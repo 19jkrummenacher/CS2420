4dc1cb171365757bb5e42b83260b4868fd86a0c6
pratx1906 <57968015+pratx1906@users.noreply.github.com>
2020-09-06 12:58:28 +0530

    Prat

--- assign02/src/assign02/Book.java
+++ assign02/src/assign02/Book.java
@@ -65,12 +65,16 @@ public class Book {
 	public boolean equals(Object other) 
 	{
 		if(!(other instanceof Book))
+		{
 			return false;
+		}
 		
 		Book otherBook = (Book)other;
 		
 		if(otherBook.getIsbn() == this.getIsbn() && otherBook.getAuthor().equals(this.getAuthor()) && otherBook.getTitle().equals(this.getTitle()))
+		{
 			return true;
+		}
 		
 		return false;
 	}
