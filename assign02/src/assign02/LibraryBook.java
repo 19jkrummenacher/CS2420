2fe8fce8f8a35b5ea5bdd9aa946d4f88f0498a43
pratx1906 <57968015+pratx1906@users.noreply.github.com>
2020-09-09 01:03:48 +0530

    Made Some changes and fixed LibraryBook

--- assign02/src/assign02/LibraryBook.java
+++ assign02/src/assign02/LibraryBook.java
@@ -1,49 +1,49 @@
 package assign02;
+
 import java.util.GregorianCalendar;
+
 /**
  * 
  * @author Jonathan and Pratyush
  *
  */
-public class LibraryBook extends Book
-{
+public class LibraryBook extends Book {
 	private String holder;
 	private GregorianCalendar dueDate;
-	
-	public LibraryBook(long isbn, String author, String title)
-	{
+
+	public LibraryBook(long isbn, String author, String title) {
 		super(isbn, author, title);
-		this.dueDate=null;
+		this.dueDate = null;
 	}
-	
+
 	/**
 	 * This reports whether the librarybook object is checked in or out.
-	 * @param g 
+	 * 
+	 * @param g
 	 * @param passHolder
 	 * 
 	 */
-	public void checkOut(GregorainCalendar g, String passHolder)
-	{
-		this.checkedOut=g;
-		this.holder=passHolder;
+	public void checkOut(GregorainCalendar g, String passHolder) {
+		this.checkedOut = g;
+		this.holder = passHolder;
 	}
-	
+
 	/**
-	 * This runs a script that resets the due date to null, holder to null, and checkedOut to false.
+	 * This runs a script that resets the due date to null, holder to null, and
+	 * checkedOut to false.
 	 */
-	public void checkIn()
-	{
+	public void checkIn() {
 		this.checkedOut = false;
 		this.dueDate = null;
-		this.holder = null;		
+		this.holder = null;
 	}
-	
+
 	/**
 	 * returns the current holder
+	 * 
 	 * @return String
 	 */
-	public String getHolder()
-	{
-		return holder; 
+	public String getHolder() {
+		return holder;
 	}
 }
